5f7b193fd8a10162828a67cb6076539c65d9464b
bwschwartz <102887694+bwschwartz@users.noreply.github.com>
2022-04-18 16:58:20 -0400

    Update prepare_tfrecord_lib.py

--- ddsp/training/data_preparation/prepare_tfrecord_lib.py
+++ ddsp/training/data_preparation/prepare_tfrecord_lib.py
@@ -60,7 +60,7 @@ def _load_audio(audio_path, sample_rate):
   audio = _load_audio_as_array(audio_path, sample_rate)
   
   # Add ground truth audio. Departure from DDSP to convert Autoencoder -> Denoising Autoencoder
-  _, end = audio_path.split('audio/')
+  _, end = audio_path.split('Estimate/')
   beginning = '/content/drive/MyDrive/Truth/' 
   true_path = os.path.join(beginning, end)
   ground_truth = _load_audio_as_array(true_path, sample_rate)
